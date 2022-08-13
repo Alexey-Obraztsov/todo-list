a0b0094e0d7a65b3ca1f297e9a2aacacdd7ee095
Alexey Obraztsov <alexlovi@mail.ru>
2022-08-13 14:08:13 +0300

    feat: add task editing

--- todo/src/components/TodoList/TodoList.js
+++ todo/src/components/TodoList/TodoList.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function TodoList({ todo, setTodo }) {
+
+    const [edit, setEdit] = useState(null)
+    const [value, setValue] = useState('')
 
     function deleteTodo(id) {
         let newTodo = [...todo].filter(item => item.id !== id)
@@ -22,6 +25,24 @@ function TodoList({ todo, setTodo }) {
     }
 
 
+    function editTodo(id, title) {
+        setEdit(id)
+        setValue(title)
+    }
+
+    function saveTodo(id) {
+        let newTodo = [...todo].map(item =>{
+            if(item.id === id) {
+                item.title = value
+            }
+            return item
+        })
+        
+        setTodo(newTodo)
+        //return to viewing mode
+        setEdit(null)
+    }
+
     console.log(todo)
 
     return (
@@ -29,9 +50,24 @@ function TodoList({ todo, setTodo }) {
             {
                 todo.map(item => (
                     <div key={item.id}>
-                        <div>{item.title}</div>
-                        <button onClick={() => deleteTodo(item.id)}>Удалить</button>
-                        <button onClick={() => statusTodo(item.id)}>Закрыть/Открыть</button>
+                        {
+                            edit === item.id ?
+                                <div>
+                                    <input value={value} onChange={(e) => setValue(e.target.value)}/>                            
+                                </div> :
+                                <div>{item.title}</div>
+                        }
+                        {
+                            edit === item.id ?
+                                <div>
+                                    <button onClick={() => saveTodo(item.id)}>Сохранить</button>
+                                </div> : 
+                                <div>
+                                    <button onClick={() => deleteTodo(item.id)}>Удалить</button>
+                                    <button onClick={() => editTodo(item.id,item.title)}>Редактировать</button>
+                                    <button onClick={() => statusTodo(item.id)}>Закрыть/Открыть</button>
+                                </div>
+                        }         
                     </div>
                 ))
             }
